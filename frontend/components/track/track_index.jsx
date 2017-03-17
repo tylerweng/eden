3b474e38317e33726379e029bdff85a8b38c45e7
Tyler Weng <tylercweng@gmail.com>
2017-03-17 00:50:58 -0700

    Update tracks to use singular React Player

--- frontend/components/track/track_index.jsx
+++ frontend/components/track/track_index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactPlayer from 'react-player';
 import TrackIndexItem from './track_index_item';
 
 class TrackIndex extends React.Component {
@@ -7,8 +8,13 @@ class TrackIndex extends React.Component {
     super(props);
 
     this.state = {
-      tracks: this.props.tracks
+      tracks: this.props.tracks,
+      playing: false,
+      trackUrl: ''
     };
+
+    this.playTrack = this.playTrack.bind(this);
+
   }
 
   componentWillMount() {
@@ -20,6 +26,13 @@ class TrackIndex extends React.Component {
     this.setState({ tracks })
   }
 
+  playTrack(trackUrl) {
+    const playing = (trackUrl === this.state.trackUrl
+                                       ? !this.state.playing
+                                       : true);
+    this.setState({ playing, trackUrl });
+  }
+
   render() {
     if(!this.state.tracks) return <div></div>;
     return (
@@ -27,8 +40,13 @@ class TrackIndex extends React.Component {
         {this.state.tracks.map(track => (
           <TrackIndexItem
             key={track.id}
-            track={track} />
+            track={track}
+            playTrack={this.playTrack}/>
         ))}
+        <ReactPlayer
+          url={this.state.trackUrl}
+          playing={this.state.playing}
+          className='react-player'/>
       </div>
     );
   }
--- frontend/components/track/track_index_item.jsx
+++ frontend/components/track/track_index_item.jsx
@@ -1,35 +1,30 @@
 import React from 'react';
-import ReactPlayer from 'react-player';
 
 class TrackIndexItem extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      playing: false
-    };
+
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  play(event) {
-    const togglePlay = !this.state.playing;
-    return event => this.setState({playing: togglePlay})
+  handleClick(event) {
+    event.preventDefault();
+    this.props.playTrack(this.props.track.track_url);
   }
+
 
   render() {
     if (!this.props.track) return <div></div>;
     const track = this.props.track;
     return (
       <button
-        onClick={this.play()}
+        onClick={this.handleClick}
         className='track-item'>
         <img
           src={`${track.img_url}`}
           alt={`${track.title}`}
           className='track-item-image' />
-        <ReactPlayer
-          url={track.track_url}
-          playing={this.state.playing}
-          className='react-player'/>
         <div className='track-item-title'>{track.title}</div>
         <div className='track-item-artist'>{track.artist}</div>
       </button>
