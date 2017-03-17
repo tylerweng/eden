b18b6652fd6e734586fafb3c349a53c12be23079
Tyler Weng <tylercweng@gmail.com>
2017-03-16 17:56:07 -0700

    Update seeds and styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,4 @@
 
 User.create(username: 'guest', password: 'password', email: 'user@gmail.com')
 Track.create(user_id: 1, title: 'All We Know', artist: 'The Chainsmokers', track_url:'https://s3-us-west-1.amazonaws.com/eden-audio/tracks/01+All+We+Know+(feat.+Phoebe+Ryan).m4a', img_url: 'https://images.genius.com/b88df63fd6fa744089bb3b07188f4249.1000x1000x1.jpg')
+Track.create(user_id: 1, title: 'Closer', artist: 'The Chainsmokers', track_url:'https://s3-us-west-1.amazonaws.com/eden-audio/tracks/01+Closer+(feat.+Halsey).m4a', img_url: 'https://images.genius.com/6e44a91448d28714d0a0f91a27193e91.1000x1000x1.jpg')
