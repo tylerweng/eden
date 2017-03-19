70b0c63a165204a92e21e9d25d3717100a237170
Tyler Weng <tylercweng@gmail.com>
2017-03-18 20:52:59 -0700

    Update styling

--- frontend/components/homepage/homepage.jsx
+++ frontend/components/homepage/homepage.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import Header from '../header/header';
-import HomepageMain from './homepage_main';
 
 const Homepage = () => (
   <div className='homepage'>
     <Header />
-    <HomepageMain />
   </div>
 );
 
