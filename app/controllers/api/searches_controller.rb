7ec7dc5401a07b20c5f32d0f4c967b9387f4886b
Tyler Weng <tylercweng@gmail.com>
2017-03-23 22:26:26 -0700

    Edit search results

--- app/controllers/api/searches_controller.rb
+++ app/controllers/api/searches_controller.rb
@@ -1,7 +1,7 @@
 class Api::SearchesController < ApplicationController
 
   def show
-    @users = User.search(params[:q])
+    # @users = User.search(params[:q])
     @tracks = Track.search(params[:q])
     render :show
   end
